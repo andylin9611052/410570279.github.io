ea0c6c5ff057bf14ff1d160b5c8eb8ab28c88f8b
andylin9611052 <andylin9611052@gmail.com>
2022-10-24 20:35:21 +0800

    commit

---
+++ all.js
@@ -0,0 +1,11 @@
+$(function name() {
+   $('.button1').on('click', function() {
+    $('.frame1').show()
+   })
+   $('.button2').on('click', function() {
+    $('.frame2').show()
+   })
+   $('.button3').on('click', function() {
+    $('.frame3').show()
+   })
+})
